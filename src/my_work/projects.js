743bf11a9ce1f728bc80182edc6e5345b80ac6a1
rahul123gautam <98rkgautam@gmail.com>
2022-08-02 17:45:17 +0530

    home

--- src/my_work/projects.js
+++ src/my_work/projects.js
@@ -3,22 +3,24 @@ export default function Project(){
     const Sort=()=>{
          window.open("https://rahul123gautam.github.io/Sorting-Visualizer-using-chartjs/");
     }
+    const Guess=()=>{
+        window.open("https://rahul123gautam.github.io/Guess-My-Number/");
+   }
     return(
         <div id="all-projects">
             <h2 className="style-project-h2" >💻Projects💻</h2>
             <div className="project" onClick={Sort}>
-                    <div className="original-project"></div>
+                    <div className="original-project one"></div>
                     <h2 className="style-project-h2">Sorting Algorithm with ChartJs</h2>
                     <p>All basic Sorting Algorithm Bubble, Selection, Insertion, Merge
                         <br/> and Quick sort are implemented and shown using Chart.Js</p>
                     <p><span className="style-project-h2">Tech stack:</span> HTML, CSS, JAVASCRIPT</p>
             </div>
 
-            <div className="project" onClick={Sort}>
-                    <div className="original-project"></div>
-                    <h2 className="style-project-h2">Sorting Algorithm with ChartJs</h2>
-                    <p>All basic Sorting Algorithm Bubble, Selection, Insertion, Merge
-                        <br/> and Quick sort are implemented and shown using Chart.Js</p>
+            <div className="project" onClick={Guess}>
+                    <div className="original-project two"></div>
+                    <h2 className="style-project-h2">Guess My Number</h2>
+                    <p>Making for fun using math.random() in javascript</p>
                     <p><span className="style-project-h2">Tech stack:</span> HTML, CSS, JAVASCRIPT</p>
             </div>
         </div>
